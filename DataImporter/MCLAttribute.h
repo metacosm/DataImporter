a5797a43b23ba66350fa3359a7c6b6f0123b573d
Chris Laprun <metacosm@gmail.com>
2012-06-21 15:45:28 +0200

    - Added missing MCLAttribute class.

---
+++ DataImporter/MCLAttribute.h
@@ -0,0 +1,7 @@
+#import "_MCLAttribute.h"
+
+@interface MCLAttribute : _MCLAttribute {
+}
+// Custom logic goes here.
++ (MCLAttribute *)attributeNamed:(NSString *)name;
+@end
---
+++ DataImporter/MCLAttribute.m
@@ -0,0 +1,10 @@
+#import "MCLAttribute.h"
+
+@implementation MCLAttribute
+
+// Custom logic goes here.
+
++ (MCLAttribute *)attributeNamed:(NSString *)name {
+  return nil;
+}
+@end
